431d94443f32ff7099e926acc1dd3a2710cf1281
Tanishka Parab <tanishka2032@outlook.com>
2025-10-17 12:06:42 +0530

    Add frontend .gitignore and remove ignored files from Git

--- frontend/project-management-ui/.next/static/chunks/project-management-ui_app_favicon_ico_mjs_918395f4._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/84f86_next_dist_34f8bc7d._.js"
-],
-    source: "dynamic"
-});
--- frontend/project-management-ui/.next/static/chunks/project-management-ui_app_page_tsx_e6728300._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/84f86_next_dist_cb7a5a78._.js"
-],
-    source: "dynamic"
-});
